1606c31978a812c15668fe60bfdf831603a34405
Samuel Servulo <sservulo@ekumenlabs.com>
2018-11-16 12:43:16 -0200

    Added python cmake block to action cmake

--- rosidl_generator_py/cmake/rosidl_generator_py_generate_action_interfaces.cmake
+++ rosidl_generator_py/cmake/rosidl_generator_py_generate_action_interfaces.cmake
@@ -191,6 +191,166 @@ target_compile_definitions(${rosidl_generate_action_interfaces_TARGET}${_target_
 #   ament_export_libraries(${rosidl_generate_action_interfaces_TARGET}${_target_suffix})
 # endif()
 
+# python block
+set(_target_suffix "_actions__py")
+
+# move custom command into a subdirectory to avoid multiple invocations on Windows
+set(_subdir
+  "${CMAKE_CURRENT_BINARY_DIR}/${rosidl_generate_action_interfaces_TARGET}${_target_suffix}")
+file(MAKE_DIRECTORY "${_subdir}")
+# TODO: custom cmake?
+file(READ "${rosidl_generator_py_DIR}/custom_command.cmake" _custom_command)
+file(WRITE "${_subdir}/CMakeLists.txt" "${_custom_command}")
+add_subdirectory("${_subdir}" ${rosidl_generate_action_interfaces_TARGET}${_target_suffix})
+set_property(
+  SOURCE
+  ${_generated_files}
+  PROPERTY GENERATED 1)
+
+macro(set_properties _build_type)
+  set_target_properties(${_target_name} PROPERTIES
+    COMPILE_OPTIONS "${_extension_compile_flags}"
+    PREFIX ""
+    LIBRARY_OUTPUT_DIRECTORY${_build_type} ${_output_path}
+    RUNTIME_OUTPUT_DIRECTORY${_build_type} ${_output_path}
+    OUTPUT_NAME "${PROJECT_NAME}_s__${_typesupports}${PythonExtra_EXTENSION_SUFFIX}"
+    SUFFIX "${PythonExtra_EXTENSION_EXTENSION}")
+endmacro()
+
+macro(set_lib_properties _build_type)
+  set_target_properties(${_target_name_lib} PROPERTIES
+    COMPILE_OPTIONS "${_extension_compile_flags}"
+    LIBRARY_OUTPUT_DIRECTORY${_build_type} ${_output_path}
+    RUNTIME_OUTPUT_DIRECTORY${_build_type} ${_output_path})
+endmacro()
+
+set(_target_name_lib "${rosidl_generate_action_interfaces_TARGET}__actions_python")
+add_library(${_target_name_lib} SHARED
+  ${_generated_files}
+)
+add_dependencies(
+  ${_target_name_lib}
+  ${rosidl_generate_action_interfaces_TARGET}${_target_suffix}
+  ${rosidl_generate_action_interfaces_TARGET}__rosidl_typesupport_c
+)
+
+target_link_libraries(
+  ${_target_name_lib}
+  ${PythonExtra_LIBRARIES}
+)
+target_include_directories(${_target_name_lib}
+  PUBLIC
+  ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_c
+  ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_py
+  ${PythonExtra_INCLUDE_DIRS}
+)
+
+rosidl_target_interfaces(${_target_name_lib}
+  ${rosidl_generate_action_interfaces_TARGET} rosidl_typesupport_c)
+
+foreach(_typesupport_impl ${_typesupports})
+  find_package(${_typesupport_impl} REQUIRED)
+
+  set(_pyext_suffix "__pyext")
+  set(_target_name "${PROJECT_NAME}__${_typesupport_impl}${_pyext_suffix}")
+
+  # TODO: extension?
+  add_library(${_target_name} SHARED
+    ${_generated_extension_${_typesupport_impl}_files}
+  )
+  add_dependencies(
+    ${_target_name}
+    ${rosidl_generate_action_interfaces_TARGET}${_target_suffix}
+    ${rosidl_generate_action_interfaces_TARGET}__rosidl_typesupport_c
+  )
+
+  set(_extension_compile_flags "")
+  set(_PYTHON_EXECUTABLE ${PYTHON_EXECUTABLE})
+  if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(_extension_compile_flags -Wall -Wextra)
+  endif()
+  if(WIN32 AND "${CMAKE_BUILD_TYPE}" STREQUAL "Debug")
+    set(PYTHON_EXECUTABLE ${PYTHON_EXECUTABLE_DEBUG})
+  endif()
+  set_properties("")
+  if(WIN32)
+    set_properties("_DEBUG")
+    set_properties("_MINSIZEREL")
+    set_properties("_RELEASE")
+    set_properties("_RELWITHDEBINFO")
+  endif()
+  target_link_libraries(
+    ${_target_name}
+    ${_target_name_lib}
+    ${PythonExtra_LIBRARIES}
+    ${rosidl_generate_action_interfaces_TARGET}__${_typesupport_impl}
+  )
+
+  target_include_directories(${_target_name}
+    PUBLIC
+    ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_c
+    ${CMAKE_CURRENT_BINARY_DIR}/rosidl_generator_py
+    ${PythonExtra_INCLUDE_DIRS}
+  )
+
+  rosidl_target_interfaces(${_target_name}
+    ${rosidl_generate_action_interfaces_TARGET} rosidl_typesupport_c)
+
+  ament_target_dependencies(${_target_name}
+    "rosidl_generator_c"
+    "rosidl_typesupport_c"
+    "rosidl_typesupport_interface"
+  )
+  foreach(_pkg_name ${rosidl_generate_action_interfaces_DEPENDENCY_PACKAGE_NAMES})
+    ament_target_dependencies(${_target_name}
+      ${_pkg_name}
+    )
+  endforeach()
+
+  add_dependencies(${_target_name}
+    ${rosidl_generate_action_interfaces_TARGET}__${_typesupport_impl}
+  )
+  ament_target_dependencies(${_target_name}
+    "rosidl_generator_c"
+    "rosidl_generator_py"
+    "${rosidl_generate_action_interfaces_TARGET}__rosidl_generator_c"
+  )
+  set(PYTHON_EXECUTABLE ${_PYTHON_EXECUTABLE})
+
+  if(NOT rosidl_generate_action_interfaces_SKIP_INSTALL)
+    install(TARGETS ${_target_name}
+      DESTINATION "${PYTHON_INSTALL_DIR}/${PROJECT_NAME}")
+  endif()
+endforeach()
+
+foreach(_pkg_name ${rosidl_generate_action_interfaces_DEPENDENCY_PACKAGE_NAMES})
+  set(_pkg_install_base "${${_pkg_name}_DIR}/../../..")
+  set(_pkg_python_libname "${_pkg_name}__python")
+
+  if(WIN32)
+    target_link_libraries(${_target_name_lib} "${_pkg_install_base}/Lib/${_pkg_python_libname}.lib")
+  elseif(APPLE)
+    target_link_libraries(${_target_name_lib} "${_pkg_install_base}/lib/lib${_pkg_python_libname}.dylib")
+  else()
+    target_link_libraries(${_target_name_lib} "${_pkg_install_base}/lib/lib${_pkg_python_libname}.so")
+  endif()
+endforeach()
+
+set_lib_properties("")
+if(WIN32)
+  set_lib_properties("_DEBUG")
+  set_lib_properties("_MINSIZEREL")
+  set_lib_properties("_RELEASE")
+  set_lib_properties("_RELWITHDEBINFO")
+endif()
+if(NOT rosidl_generate_interfaces_SKIP_INSTALL)
+  install(TARGETS ${_target_name_lib}
+    ARCHIVE DESTINATION lib
+    LIBRARY DESTINATION lib
+    RUNTIME DESTINATION bin)
+endif()
+
+# lint checks
 if(BUILD_TESTING AND rosidl_generate_interfaces_ADD_LINTER_TESTS)
   if(NOT _generatedfiles STREQUAL "")
     # c tests?
